8e1a13beaa175a1f0fe6fd99ba99b28aa8af7109
name <tzolcubekov@gmail.com>
2025-11-19 13:40:10 +0600

    Improved HandleRequest method in Main class

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ import server.Server;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try{
+        try {
             HttpServer server = Server.makeServer();
             initRoutes(server);
             server.start();
@@ -28,42 +28,54 @@ public class Main {
         server.createContext("/apps/profile", Main::handleRequest);
     }
 
-    private static void handleRequest(HttpExchange exchange){
-        try{
-            exchange.getRequestHeaders().add("Content-Type", "text/plain; charset=utf-8");
-            int responseCode = 200;
-            int length = 0;
-            exchange.sendResponseHeaders(responseCode, length);
-
-            try(PrintWriter writer = getWriterFrom(exchange)){
+    private static void handleRequest(HttpExchange exchange) {
+        try {
+            StringWriter sw = new StringWriter();
+            try (PrintWriter writer = new PrintWriter(sw)) {
                 String method = exchange.getRequestMethod();
                 URI uri = exchange.getRequestURI();
                 String path = exchange.getHttpContext().getPath();
 
                 write(writer, "HTTP Method", method);
                 write(writer, "Request", uri.toString());
-                write(writer, "Handler", path);
+                write(writer, "Handler Path", path);
                 writeHeaders(writer, "Request Headers", exchange.getRequestHeaders());
-                writer.flush();
             }
-        } catch (IOException e){
+
+            String response = sw.toString();
+            byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+            int responseLength = responseBytes.length;
+            int responseCode = 200;
+
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
+            exchange.sendResponseHeaders(responseCode, responseLength);
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(responseBytes);
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            exchange.close();
         }
     }
 
     private static void writeHeaders(PrintWriter writer, String type, Headers requestHeaders) {
-        write(writer,type, "");
-        requestHeaders.forEach((k,v) -> write(writer,"\t"+k,v.toString()));
+        write(writer, type, "");
+        requestHeaders.forEach((k, v) -> write(writer, "\t" + k, v.toString()));
     }
 
-    private static void write(PrintWriter writer, String msg, String method) {
-        String data = String.format("%s : %s%n", method, msg);
+    private static void write(PrintWriter writer, String label, String value) {
+        String data = String.format("%s : %s%n", label, value);
         writer.write(data);
     }
 
     private static PrintWriter getWriterFrom(HttpExchange exchange){
         OutputStream os = exchange.getResponseBody();
         Charset charset = StandardCharsets.UTF_8;
-        return new PrintWriter(new OutputStreamWriter(os, charset));
+        return new PrintWriter(os, false, charset);
     }
 }
+
+
--- src/server/Server.java
+++ src/server/Server.java
@@ -4,16 +4,17 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.sql.SQLOutput;
 
 public class Server {
-    private Server(){}
+    private Server(){
+
+    }
 
     public static HttpServer makeServer() throws IOException {
         String host = "localhost";
         InetSocketAddress address = new InetSocketAddress(host, 8089);
 
-        System.out.format("Server started at http://%s:%s%n", address.getHostName(), address.getPort());
+        System.out.format("Server started at http://%s:%s.%n", address.getHostName(), address.getPort());
         HttpServer server = HttpServer.create(address, 50);
         System.out.println("\t\tDone!");
         return server;
